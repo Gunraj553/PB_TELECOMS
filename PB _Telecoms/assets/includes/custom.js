956dc8c5bf925fdb3844b7a49df342c5d58d5755
Gunraj553 <gsodhi1557@hotmail.com>
2023-11-17 17:45:47 -0500

    latest commit by gunraj

--- PB _Telecoms/assets/includes/custom.js
+++ PB _Telecoms/assets/includes/custom.js
@@ -8,14 +8,6 @@ jQuery(document).ready(function ($) {
     //     autoPlay: 4000
     // });
     $(".all-banner-slide").owlCarousel({
-        // items: 1,
-        // smartSpeed: 1000,
-        // autoplay: true,
-        // lazyLoad: true,
-        // dots: true,
-        // autoplayTimeout: 3000,
-        // singleItem: true,
-        // navigation:true
         dots: true,
         slideSpeed: 300,
         paginationSpeed: 500,
