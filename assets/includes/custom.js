baa52b6141dda506f50074c031669b02d038896b
Gunraj553 <gsodhi1557@hotmail.com>
2023-12-05 20:46:00 -0500

    changes by gunraj

--- assets/includes/custom.js
+++ assets/includes/custom.js
@@ -49,10 +49,6 @@ function validate_login_Form() {
 
     if (email && password) {
         loginUser();
-        // index_db();
-        // event.preventDefault();
-        // login(email,password);
-
     }
 
 }
@@ -110,26 +106,114 @@ function validate_signup_Form() {
         return false;
     }
 
-
-
     // Redirect if valid
     if (email && password && confirmPassword && (password === confirmPassword) && password.length >= 8 && name) {
         signupUser();
-        // index_db();
-        // window.location.href = 'index.html';
-        // event.preventDefault();
-        // signUp(email,name,password);
 
     }
 
-
 }
-
  
 
 
+function validate_checkout_form(){
 
+    document.getElementById("first_name_Error").innerHTML='';
+    document.getElementById("middle_name_Error").innerHTML = '';
+    document.getElementById("last_name_Error").innerHTML = '';
+    document.getElementById("city_Error").innerHTML = '';
+    document.getElementById("post_Error").innerHTML = '';
+    document.getElementById("street_Error").innerHTML = '';
+    document.getElementById("phone_Error").innerHTML = '';
+    document.getElementById("email_Error").innerHTML = '';
+    document.getElementById("country_Error").innerHTML = '';
+    document.getElementById("state_Error").innerHTML = '';
 
+    document.getElementById("first_name").classList.remove('error-border');
+    document.getElementById("middle_name").classList.remove('error-border'); 
+    document.getElementById("last_name").classList.remove('error-border');
+    document.getElementById("city").classList.remove('error-border');
+    document.getElementById("post").classList.remove('error-border');
+    document.getElementById("street").classList.remove('error-border');
+    document.getElementById("phone").classList.remove('error-border');
+    document.getElementById("email").classList.remove('error-border');
+    document.getElementById("country").classList.remove('error-border');
+    document.getElementById("state").classList.remove("error-border");
+
+    var first_name = document.getElementById("first_name").value.trim();
+    var middle_name = document.getElementById("middle_name").value.trim();
+    var last_name = document.getElementById("last_name").value.trim();
+    var city = document.getElementById("city").value.trim();
+    var post = document.getElementById("post").value.trim();
+    var street = document.getElementById("street").value.trim();
+    var phone = document.getElementById("phone").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var country =  document.getElementById("country").value.trim();
+    var state = document.getElementById("state").value.trim();
+
+    if (!first_name) {
+        document.getElementById('first_name_Error').innerHTML = 'First name is required';
+        document.getElementById('first_name').classList.add('error-border');
+        return false;
+    }
+    if (!middle_name) {
+        document.getElementById('middle_name_Error').innerHTML = 'Middle name is required';
+        document.getElementById('middle_name').classList.add('error-border');
+        return false;
+    }
+    if (!last_name) {
+        document.getElementById('last_name_Error').innerHTML = 'Last name is required';
+        document.getElementById('last_name').classList.add('error-border');
+        return false;
+    }
+
+    if (!country) {
+        document.getElementById('country_Error').innerHTML = 'Country is required';
+        document.getElementById('country').classList.add('error-border');
+        return false;
+    }
+    if (!state) {
+        document.getElementById('state_Error').innerHTML = 'State is required';
+        document.getElementById('state').classList.add('error-border');
+        return false;
+    }
+
+    if (!city) {
+        document.getElementById('city_Error').innerHTML = 'City is required';
+        document.getElementById('city').classList.add('error-border');
+        return false;
+    }
+
+    if (!post) {
+        document.getElementById('post_Error').innerHTML = 'Postal code is required';
+        document.getElementById('post').classList.add('error-border');
+        return false;
+    }
+
+    if (!street) {
+        document.getElementById('street_Error').innerHTML = 'Street is required';
+        document.getElementById('street').classList.add('error-border');
+        return false;
+    }
+
+    if (!phone) {
+        document.getElementById('phone_Error').innerHTML = 'Phone is required';
+        document.getElementById('phone').classList.add('error-border');
+        return false;
+    }
+
+    if (!email) {
+        document.getElementById('email_Error').innerHTML = 'Email is required';
+        document.getElementById('email').classList.add('error-border');
+        return false;
+    }
+  
+
+    if (first_name && last_name && middle_name && city && post && street && phone && email && country && state){
+        alert("Order Placed Thank you !!!")
+        window.href("index.html");
+    }
+}
 
 let db;
 
@@ -237,8 +321,7 @@ function authenticateUser(email, password) {
 
         let user = request.result;
         // console.log(user.name);
-       
-        
+               
 
         if (user && user.password === password) {
             if (user){
@@ -246,6 +329,8 @@ function authenticateUser(email, password) {
             }else{
                 sessionStorage.setItem("loggedInUser", JSON.stringify({ name: '', email: user.email }));
             }
+
+
             console.log("login");
             alert("Login successful!");
            
